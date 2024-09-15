ebface57c59cba145c17f109f1df05a6664e3fd3
Taylor Ng <taylormng95@gmail.com>
2024-09-14 20:02:24 -0400

    completes lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print('The value must be a string.')
+
+    def is_sentence(self):
+        return self.value[-1] == '.'
+    
+    def is_question(self):
+        return self.value[-1] == '?'
+    
+    def is_exclamation(self):
+        return self.value[-1] == '!'
+    
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        new_value = self.value.replace('!', '.')
+        new_value = new_value.replace('?', '.')
+        arr = new_value.split('. ')
+        return len(arr)
